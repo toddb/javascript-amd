87fd0527a1cebcba2790323c43c85d69935d0a46
toddb <todd-github@goneopen.com>
2012-06-28 21:55:00 +1200

    Refactor to put tests in jasmine namespace

--- test/utils/spec_functional-test-spike.js
+++ test/utils/spec_functional-test-spike.js
@@ -1,47 +1,93 @@
-var logger = jasmine.createSpy('logger'), 
-  _,
-  request,
-  context,
-  expectations = []
+// TODO: modularise
+_requires( 'underscore', function(u){ 
+  _ = u
   
-_requires( 'underscore', function(u){ _ = u })()
+  var mime = {
+    Json: 'application/json'
+  }
+  var response = {
+    OK: 200
+  }
+  
+  // setup the helpers in global namespace
+  _.each(_.extend(mime,response), function(val, key){ window[key] = val})  
+  
+  // setup the jasmine.Spec methods in global namespace
+  _.each(['returning', 'showing', 'gets'], function( val ){
+    window[val] = function(){
+      return jasmine.getEnv().currentSpec[val](arguments);
+      if (isCommonJS) exports[val] = window[val];
+    } 
+  })
 
-function check(){
-  
-  var deferred, 
-    widget, 
-    context = arguments[0];
-  
-  _requires( 'utils/deferredBefore', function(deferred){ 
-      $.DEFAULT_DEFERRED = deferred(context, logger); 
-  })()
+  // setup the jasmine.Env methods in global namespace
+  _.each(['load'], function( val ){
+    window[val] = function(desc, func){
+      return jasmine.getEnv()[val](desc, func);
+      if (isCommonJS) exports[val] = window[val];
+    } 
+  }) 
+   
+})()
 
-  _requires( context, function(w){ 
-      widget = w
-  })()
-  
-  waitsFor(function(){
-    return logger.callCount >= expectations.length
-  }, 'all ' + expectations.length + ' expectation(s) to be executed but only ' + logger.callCount + ' completed')
-  
-  runs(function(){
-    _(expectations.length).times(function(){
-      expectations.shift()( context )
-    })
 
-    $(widget).remove()    
+var load = function(desc, func) {
+  return jasmine.getEnv()['load'](desc, func);
+};
+if (isCommonJS) exports.load = load;
+
+jasmine.Env.prototype.load = function(context, func) {
+  var spec = new jasmine.Spec(this, this.currentSuite, context);
+  this.currentSuite.add(spec);
+  this.currentSpec = spec;
+  
+  // always load up the deferred with the expectation spies on a load
+  this.beforeEach(function(){
+    this.logger = jasmine.createSpy('deferred-spy-for-testing');
+    this.expectations = []
+    var that = this
+    _requires( 'utils/deferredBefore', function(deferred){ 
+        $.DEFAULT_DEFERRED = deferred(context, that.logger); 
+    })()
   })
   
-}
+  this.afterEach(function(){
+    $.DEFAULT_DEFERRED = null
+  })
+  
+  if (func) {
+    spec.runs(func);
+  }
+  
+  spec.runs(function(){
+    
+      _requires( context, function(widget){ 
+                 
+        waitsFor(function(){
+          return spec.logger.callCount >= spec.expectations.length
+        }, 'all ' + spec.expectations.length + ' expectation(s) to be executed but only ' + spec.logger.callCount + ' completed')
 
-var OK = 200;
-var Json = 'application/json';
+        runs(function(){
+          _(spec.expectations.length).times(function(){
+            spec.expectations.shift()( context )
+          })         
+          $(widget).remove()              
+        })
 
-function GET(){
-  var url = arguments[0]
-    accept = arguments[1],
-    status = arguments[2][0],
-    responseText = arguments[2][1]
+      })()
+     
+  });
+
+  return spec;
+};
+
+jasmine.Spec.prototype.gets = function (args) {
+  
+  // TODO: cleanup this when needed
+  var url = args[0]
+    accept = args[1],
+    status = args[2][0],
+    responseText = args[2][1]
   
   $.mockjax( {
     url: url,
@@ -51,14 +97,27 @@ function GET(){
     responseText: responseText
   })
   
-  expectations.push( function( context ){
-    expect(logger).toHaveBeenCalledWith( [context, "GET", url, status] );
+  var that = this
+  this.expectations.push( function( context ){
+    expect(that.logger).toHaveBeenCalledWith( [context, "GET", url, status] );
   })
   
+  // var block = new jasmine.Block(this.env, func, this);
+  // this.addToQueue(block);
+  
+  return this;
+};
+
+jasmine.Spec.prototype.showing = function (args) {
+  var args = _.toArray(args)
+  var that = this
+  this.expectations.push( function( context ){
+    expect(that.logger).toHaveBeenCalledWith( _.flatten([context, args]));
+  })
 }
 
-function returns(){
-  return _(_.toArray(arguments)).map(function( val ){
+jasmine.Spec.prototype.returning = function (args) {
+  return _(args).map(function( val ){
     var ret
     if( _.isString(val) && val.match(/json!|html!|text!/)){
       _requires( val, function( r ){ 
@@ -71,37 +130,19 @@ function returns(){
   })
 }
 
-function shows(){
-  var args = _.toArray(arguments)
-  expectations.push( function( context ){
-    expect(logger).toHaveBeenCalledWith( _.flatten([context, args]));
-  })
-}
-
 describe("functional test", function() {
   
   beforeEach(function() {
     $('<link rel="collection" type="application/json" href="http://localhost:8888/orders/current">').prependTo('HEAD')
   });
 
-  it("should resolve with - code with comments and extra code", function() {
-    check(
-      'rest-coffee-deferred/main',
-      GET('http://localhost:8888/orders/current', Json, returns(OK, 'json!server/orders/current.json')),
-      shows('text!coffee/views/index.html', 'text!coffee/views/_item.html', 'text!coffee/views/_new.html' ),
-      GET('http://localhost:8888/orders/1', Json, returns(OK, 'json!server/orders/1.json')),
-      GET('http://localhost:8888/orders/2', Json, returns(OK, 'json!server/orders/1.json')),
-      GET('http://localhost:8888/orders/3', Json, returns(OK, 'json!server/orders/1.json'))
-    )
-  });
-
-  // short-hand versions if needed
-  // xit("should resolve with - code with comments and extra code -- short version", function() {
-  //   check(
-  //     'rest-coffee-deferred/main',
-  //     GET('*/orders/current', returns('json!server/orders/current.json')),
-  //     shows('text!coffee/views/index.html')
-  //   )
-  // });
+  load('rest-coffee-deferred/main', function(){
+      gets('http://localhost:8888/orders/current', Json, returning(OK, 'json!server/orders/current.json'))
+      showing('text!coffee/views/index.html', 'text!coffee/views/_item.html', 'text!coffee/views/_new.html')
+      gets('http://localhost:8888/orders/1', Json, returning(OK, 'json!server/orders/1.json'))
+      gets('http://localhost:8888/orders/2', Json, returning(OK, 'json!server/orders/1.json'))
+      gets('http://localhost:8888/orders/3', Json, returning(OK, 'json!server/orders/1.json')) 
+    }
+  )
 
 });
